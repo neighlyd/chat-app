9eb04bd7bf0ce2b7175f3d4664e570c6f4308527
Dustin Neighly <neighlyd@gmail.com>
2019-03-25 14:08:21 -0700

    Began templating

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,70 @@
 const socket = io()
 
+// Elements
+
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $shareLocationButton = document.querySelector('#share-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+
 socket.on('message', (message) => {
-    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+socket.on('locationMessage', (url) => {
+    const html = Mustache.render(locationMessageTemplate, {
+        url
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    let message = document.querySelector('input')
-    socket.emit('sendMessage', message.value)
-    message.value = ''
+    // disable button
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error){
+            return console.log(error)
+        }
+        console.log('Message delivered')
+    })
+    
+})
+
+$shareLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Your browser does not support geolocation')
+    }
+
+    $shareLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('shareLocation', {
+            lat: position.coords.latitude, 
+            long: position.coords.longitude
+        }, () => {
+            $shareLocationButton.removeAttribute('disabled')
+            console.log('Location Received')
+        })
+    })
 })
 
 // socket.on('countUpdated', (count) => {
--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
 const path = require('path')
-const express = require('express')
 const http = require('http')
+const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
 const server = http.createServer(app)
 const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
 const welcome = 'Welcome to the server'
+const filter = new Filter()
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', welcome)
+    socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        if (filter.isProfane(message)){
+            return callback('Profanity is not allowed')    
+        }
+
         io.emit('message', message)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
+
+    socket.on('shareLocation', (coords, callback) => {
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.lat},${coords.long}`)
+        callback()
     })
 })
 
