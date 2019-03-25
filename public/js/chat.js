5903a984c41ae5b70e0094e8a795f9e6147fa08c
Dustin Neighly <neighlyd@gmail.com>
2019-03-25 13:09:08 -0700

    Added socket chat functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,20 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let message = document.querySelector('input')
+    socket.emit('sendMessage', message.value)
+    message.value = ''
+})
+
+// socket.on('countUpdated', (count) => {
+//     console.log(`The count has been updated to: ${count}`)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     socket.emit('increment')
+// })
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-const app = require('./app')
+const path = require('path')
+const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 
+const app = express()
 const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 const server = http.createServer(app)
 const io = socketio(server)
 
-io.on('connection', () => {
+app.use(express.static(publicDirectoryPath))
+
+const welcome = 'Welcome to the server'
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    socket.emit('message', welcome)
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
 })
 
 server.listen(port, () => {
